a33b6d617f1ce3d8b13513f681cf1dd167f47aa7
FrancoMascardi <101577478+FrancoMascardi@users.noreply.github.com>
2022-03-29 10:41:54 -0300

    Finish exercise 1

--- Assets/Scripts/Ejercicio_1.cs
+++ Assets/Scripts/Ejercicio_1.cs
@@ -7,6 +7,12 @@ public class Ejercicio_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int num1;
+        num1 = 0;
+        num1 = num1 + 2;
+        Debug.Log("El numero es" + num1);
+        num1 *= num1;
+        Debug.Log("El numero es" + num1);
         
     }
 
