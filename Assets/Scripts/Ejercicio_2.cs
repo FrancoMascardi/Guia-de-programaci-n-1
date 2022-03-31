ba78549b0bcf128d6568e15dbc06a33e205cc456
FrancoMascardi <101577478+FrancoMascardi@users.noreply.github.com>
2022-03-31 08:42:47 -0300

    Finish exercise 2

---
+++ Assets/Scripts/Ejercicio_2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5, respectivamente, realice la operación correspondiente y muestre el resultado en pantalla:
+// a.Suma
+// b.Producto
+public class Ejercicio_2 : MonoBehaviour
+{
+    public int num1 = 4;
+    public int num2 = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(num1 + num2);
+        Debug.Log(num1 * num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
