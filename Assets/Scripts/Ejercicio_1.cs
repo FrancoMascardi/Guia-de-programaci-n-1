0a581c57940742d32ba17f0a068d35366e9cbea2
FrancoMascardi <101577478+FrancoMascardi@users.noreply.github.com>
2022-04-04 14:01:28 -0300

    Exercise 5 finished.

--- Assets/Scripts/Ejercicio_1.cs
+++ Assets/Scripts/Ejercicio_1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente  valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el  resultado de multiplicarlo por sí mismo. 
+
 public class Ejercicio_1 : MonoBehaviour
 {
     // Start is called before the first frame update
---
+++ Assets/Scripts/Ejercicio_4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//4.. Realizá un programa que permita ingresar dos números enteros num1 y num2 por
+//Inspector. Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena
+//utilizando el siguiente formato: “La suma entre “ + num1 + “ y “ + num2 + “ da como
+//resultado “ + num1 + num2.
+public class Ejercicio_4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
